865913aad41d9dd8549b3dd9b5a389814aea943e
nandan <nandan07@outlook.com>
2015-05-29 12:33:53 +0530

    changed add

--- 49_prime_permutation.py
+++ 49_prime_permutation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-import math
-#import ipdb
-#import helper
+import ipdb
+import itertools
+import helper
 
 def isPrime(x):
     x=int(x)
@@ -17,18 +17,43 @@ def nextPrime(p):
         p+=1
         if isPrime(p):
             return p
+def getdigitkey(n):
+    digit = "".join(sorted(str(n), reverse=True))
+    return int(digit)
+#onlt consider the concecutive numbers.. 
+def isAP(ar,l):
+    cd={}
+    for i in range(len(ar)-1):
+        key=ar[i+1]-ar[i]
+        cd.setdefault(key,set())
+        cd[key].add(ar[i])
+        cd[key].add(ar[i+1])
+    ipdb.set_trace()
+    for x in cd:
+        if len(cd[x])==l:
+            return sorted(cd[x])
 
 
-#n,testdata=helper.readData("input")
-n=int(input())
-for x in range(n):
-    #n=int(testdata[x])
-    n=int(input())
-    ans=0
-    p=1
-    while(p<n):
-        sq=int(math.sqrt((n-p)/2))
-        if isPrime(p) and (n==p+2*(sq**2)):
-            ans+=1
-        p=nextPrime(p)
-    print(ans)
+
+n,testdata=helper.readData("input")
+#n=int(testdata[x])
+N,K=testdata[0].split()
+N=int(N)
+K=int(K)
+prime={}
+ar=[1487, 1847, 4817, 4871, 7481, 7841, 8147, 8741]
+isAP(ar,K)
+for n in range(10,N+1):
+    if isPrime(n):
+        prime.setdefault(getdigitkey(n),set())
+for key in prime:
+    num=itertools.permutations(sorted(str(key)))
+    for p in num:
+        p=int("".join(p))
+        if len(str(p))==len(str(key)):
+            if isPrime(p):
+                prime[key].add(p)
+    if len(prime[key])>=K:
+       ar=sorted(prime[key])
+
+ipdb.set_trace()
