650aa7dd33beaa812c996629aa81b6ceaf956a03
nandan <nandan07@outlook.com>
2015-06-06 23:52:03 +0530

    optimised solution

--- 23_abundant_sum.py
+++ 23_abundant_sum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#import ipdb
-#import helper
+import ipdb
+import helper
 
 def factors_sum(x):
     x=int(x)
@@ -16,25 +16,31 @@ def factors_sum(x):
         su+=f
     return int(su)
 
-abundant_ar=[]
+abundant_ar=[0]
 abundant_set=set()
 for i in range(1,28125):
     if factors_sum(i)>i:
-        abundant_set.add(i)
-        abundant_ar.append(i)
+        abundant_ar.append(1)
+    else:
+        abundant_ar.append(0)
 n=int(input())
 #n,testdata=helper.readData("input")
 for i in range(n):
     N=int(input())
+    #N=int(testdata[i])
     find=False
     if N>28123:
         print("YES")
         continue
-    for j in abundant_ar:
+    for j in range(1,len(abundant_ar)):
+        #ipdb.set_trace()
         if j<N:
-            if N-j in abundant_set:
-                find=True
-                break
+            if abundant_ar[j]==1:
+                if abundant_ar[N-j]==1:
+                    find=True
+                    break
+            else:
+                continue
         else:
             break
     if find:
